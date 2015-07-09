e7013d45d6fd619a0afde6efbb4f27d11f1e7362
mikehajj <mike.hajj@gmail.com>
2015-07-09 15:45:07 +0300

    minor changes in the output of runService.sh

--- FILES/scripts/runService.sh
+++ FILES/scripts/runService.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 export SOAJS_SRVIP=$(/sbin/ip route|awk '/172.17.0.0/ {print $9}')
-echo $SOAJS_SRVIP
-echo $SOAJS_ENV
-echo $SOAJS_PROFILE
+export $HOSTNAME=hostname
+
+echo "Info:"
+echo "====="
+echo "* HOSTNAME: $HOSTNAME"
+echo "* IP ADDRESS: $SOAJS_SRVIP"
+echo "* ENVIRONMENT: $SOAJS_ENV"
+echo "* PROFILE: $SOAJS_PROFILE"
+
+if [ -n "$SOAJS_GC_NAME" ]; then
 echo $SOAJS_GC_VERSION
 echo $SOAJS_GC_NAME
+fi
+
+echo "--------------------------------------------------------------------------------------------"
+echo "Logs:"
+echo "====="
 node $1
