d97668c726fb0074eec33dc5a7aa34179bb94bf5
antoinehage <me@antoinehage.com>
2016-02-24 09:09:44 -0500

    deployer version 0

--- FILES/scripts/runService.sh
+++ FILES/scripts/runService.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 service=$1
-
+IP_SUBNET=$3
+SET_SOAJS_SRVIP=$2
 function serviceSuccess()
 {
     echo "service config preparation done successfully"
 
-    export SOAJS_SRVIP=$(/sbin/ip route|awk '/10.0.0.0/ {print $9}')
-    echo $SOAJS_SRVIP
+    if [ ${SET_SOAJS_SRVIP} == "on" ]; then
+        export SOAJS_SRVIP=$(/sbin/ip route|awk '/${IP_SUBNET}/ {print $9}')
+        echo $SOAJS_SRVIP
+    fi
     echo $SOAJS_ENV
     echo $SOAJS_PROFILE
 
