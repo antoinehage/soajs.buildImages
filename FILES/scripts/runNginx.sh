b3f78844d280e03159bf84d7815a5186a7281f91
antoinehage <me@antoinehage.com>
2016-03-30 18:39:35 -0400

    added verbose logging for deployer scripts

--- FILES/scripts/runNginx.sh
+++ FILES/scripts/runNginx.sh
@@ -5,44 +5,45 @@
 [ ${SOAJS_GIT_DASHBOARD_BRANCH} ] && dashboardDeployment=1 || dashboardDeployment=0
 function nxSuccess()
 {
-    echo "nginx config preparation done successfully"
+    echo $'\n- SOAJS Deployer preparing nginx ... '
+    echo "- Nginx config preparation done successfully"
 
     mkdir -p ${nxSitePath}
     mkdir -p ${nxSitePath}"_tmp"
 
-    pushd ${nxSitePath}"_tmp"
+    pushd ${nxSitePath}"_tmp" > /dev/null 2>&1
 
     if [ ${dashboardDeployment} == 1 ]; then
         git clone -b ${SOAJS_GIT_DASHBOARD_BRANCH} https://github.com/soajs/soajs.dashboard.git
         cp -Rf ${nxSitePath}"_tmp/"soajs.dashboard/ui/*  ${nxSitePath}"/"
-        echo "deployed dashboard UI"
+        echo "    ... deployed dashboard UI"
     fi
 
     if [ ${SOAJS_GIT_REPO} ] && [ ${SOAJS_GIT_OWNER} ]; then
         if [ ${SOAJS_GIT_TOKEN} ]; then
-            echo "Deploy from github private repo"
+            echo "- Deploying from github private repo"
             git clone -b ${BRANCH} https://${SOAJS_GIT_TOKEN}@github.com/${SOAJS_GIT_OWNER}/${SOAJS_GIT_REPO}.git
             cp -Rf ${nxSitePath}"_tmp/"${SOAJS_GIT_REPO}/*  ${nxSitePath}"/"
         else
-            echo "Deploy from github public repo"
+            echo "- Deploying from github public repo"
             git clone -b ${BRANCH} https://github.com/${SOAJS_GIT_OWNER}/${SOAJS_GIT_REPO}.git
             cp -Rf ${nxSitePath}"_tmp/"${SOAJS_GIT_REPO}/*  ${nxSitePath}"/"
         fi
-        echo "deployed additional custom dashboard UI"
     else
-        echo "no additional custom dashboard UI to deploy"
+        echo "- No additional custom site UI to deploy"
     fi
-    popd
+    popd > /dev/null 2>&1
     rm -Rf ${nxSitePath}"_tmp"
 
-    echo "starting nginx ..."
+        echo $'\n- SOAJS Deployer starting nginx ... '
     service nginx start
 }
 function nxFailure()
 {
-    echo "nginx config preparation failed"
+    echo "ERROR: nginx config preparation failed"
 }
 
+echo $'\n- SOAJS Deployer building the needed configuration ... '
 node /opt/soajs/FILES/deployer/nginx.js &
 b=$!
 wait $b && nxSuccess || nxFailure
--- FILES/scripts/runService.sh
+++ FILES/scripts/runService.sh
@@ -9,34 +9,38 @@ DEPLOY_FOLDER="/opt/soajs/node_modules/"
 
 function serviceSuccess()
 {
-    echo "service config preparation done successfully"
+    echo $'\n- SOAJS Deployer preparing service ... '
+    echo "- Service environment variables:"
 
     if [ ${SET_SOAJS_SRVIP} == "on" ]; then
         export SOAJS_SRVIP=$(/sbin/ip route|awk '/'${IP_SUBNET}'/ {print $9}')
         echo $SOAJS_SRVIP
     fi
-    echo $SOAJS_ENV
-    echo $SOAJS_PROFILE
+    echo "    SOAJS_ENV="$SOAJS_ENV
+    echo "    SOAJS_PROFILE="$SOAJS_PROFILE
 
     if [ -n "$SOAJS_GC_NAME" ]; then
-        echo $SOAJS_GC_VERSION
-        echo $SOAJS_GC_NAME
+        echo "    SOAJS_GC_VERSION="$SOAJS_GC_VERSION
+        echo "    SOAJS_GC_NAME="$SOAJS_GC_NAME
     fi
 
     if [ ${SOAJS_GIT_REPO} ] && [ ${SOAJS_GIT_OWNER} ]; then
-        pushd ${DEPLOY_FOLDER}
+        pushd ${DEPLOY_FOLDER} > /dev/null 2>&1
         if [ ${SOAJS_GIT_TOKEN} ]; then
-            echo "Deploy from github private repo"
+            echo "- Deploy from github private repo"
             git clone -b ${BRANCH} https://${SOAJS_GIT_TOKEN}@github.com/${SOAJS_GIT_OWNER}/${SOAJS_GIT_REPO}.git
         else
-            echo "Deploy from github public repo"
+            echo "- Deploy from github public repo"
             git clone -b ${BRANCH} https://github.com/${SOAJS_GIT_OWNER}/${SOAJS_GIT_REPO}.git
         fi
-        popd
-        echo "about to run service ${DEPLOY_FOLDER}${SOAJS_GIT_REPO}${WHAT_TO_RUN}"
-        pushd ${DEPLOY_FOLDER}${SOAJS_GIT_REPO}
-        npm install
-        popd
+        popd > /dev/null 2>&1
+
+        echo $'\n- SOAJS Deployer starting service ... '
+        echo "    -->    ${DEPLOY_FOLDER}${SOAJS_GIT_REPO}${WHAT_TO_RUN}"
+
+        pushd ${DEPLOY_FOLDER}${SOAJS_GIT_REPO} > /dev/null 2>&1
+        npm install > /dev/null 2>&1
+        popd > /dev/null 2>&1
         node ${DEPLOY_FOLDER}${SOAJS_GIT_REPO}${WHAT_TO_RUN}
     else
         echo "ERROR: unable to find environment variable SOAJS_GIT_REPO or SOAJS_GIT_OWNER. nothing to deploy"
@@ -44,9 +48,10 @@ function serviceSuccess()
 }
 function serviceFailure()
 {
-    echo "service config preparation failed"
+    echo "ERROR: service config preparation failed .... exiting :( !"
 }
 
+echo $'\n- SOAJS Deployer building the needed PROFILE ... '
 node /opt/soajs/FILES/deployer/profile.js &
 b=$!
 wait $b && serviceSuccess || serviceFailure
