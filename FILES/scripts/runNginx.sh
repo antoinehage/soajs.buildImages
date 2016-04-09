b27a7e9efb3a22d9ebb9ac26b1d2e99bfffdd5d7
antoinehage <me@antoinehage.com>
2016-04-09 08:48:22 -0400

    supporting edge case until we roll a perm solution soa.js in repos

--- FILES/scripts/runNginx.sh
+++ FILES/scripts/runNginx.sh
@@ -23,11 +23,15 @@ function nxSuccess()
         if [ ${SOAJS_GIT_TOKEN} ]; then
             echo "- Deploying from github private repo"
             git clone -b ${BRANCH} https://${SOAJS_GIT_TOKEN}@github.com/${SOAJS_GIT_OWNER}/${SOAJS_GIT_REPO}.git
-            cp -Rf ${nxSitePath}"_tmp/"${SOAJS_GIT_REPO}/ui/*  ${nxSitePath}"/"
         else
             echo "- Deploying from github public repo"
             git clone -b ${BRANCH} https://github.com/${SOAJS_GIT_OWNER}/${SOAJS_GIT_REPO}.git
+        fi
+
+        if [ -d "${nxSitePath}_tmp/${SOAJS_GIT_REPO}/ui" ]; then
             cp -Rf ${nxSitePath}"_tmp/"${SOAJS_GIT_REPO}/ui/*  ${nxSitePath}"/"
+        else
+            cp -Rf ${nxSitePath}"_tmp/"${SOAJS_GIT_REPO}/*  ${nxSitePath}"/"
         fi
     else
         echo "- No additional custom site UI to deploy"
