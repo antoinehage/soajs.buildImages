2a07eff688a967179a9297083d7e9a1bf40c3af1
ameer <faraj.ameer@gmail.com>
2017-03-17 17:39:35 +0200

    Added echo for node command that starts the service in soajsDeployer.sh

--- FILES/deployer/soajsDeployer.sh
+++ FILES/deployer/soajsDeployer.sh
@@ -207,6 +207,8 @@ function serviceRun() {
     #TODO: merge all clean calls into one function
     local SOAJS_GIT_REPO_CLEAN=$(echo ${SOAJS_GIT_REPO} | sed -e 's/[\\/\*\?"<>\|,\.-]/_/g' | awk '{print tolower($0)}')
     local SOAJS_HA_NAME_CLEAN=$(echo ${SOAJS_HA_NAME} | cut -d "." -f 1,2 | sed -e 's/[\\/\*\?"<>\|,\.-]/_/g' | awk '{print tolower($0)}')
+    
+    echo 'node '${NODE_PARAMS}' '${DEPLOY_FOLDER}${SOAJS_GIT_REPO}${MAIN}' 2>&1 | tee /var/log/soajs/'${SOAJS_ENV}'-'${SOAJS_GIT_REPO_CLEAN}'-'${SOAJS_HA_NAME_CLEAN}'-service.log'
     node ${NODE_PARAMS} ${DEPLOY_FOLDER}${SOAJS_GIT_REPO}${MAIN} 2>&1 | tee /var/log/soajs/${SOAJS_ENV}-${SOAJS_GIT_REPO_CLEAN}-${SOAJS_HA_NAME_CLEAN}-service.log
 }
 function serviceCode() {
