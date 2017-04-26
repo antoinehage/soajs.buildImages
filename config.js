b0600b61421ee18da48ea1cdfa2f115f4c6d2d6a
ameer <faraj.ameer@gmail.com>
2017-04-26 18:22:50 +0300

    Updated base, soajs, and nginx images

--- config.js
+++ config.js
@@ -17,7 +17,6 @@ module.exports = {
             "from": 'FROM soajsorg/basenginx',
             "maintainer": 'MAINTAINER SOAJS Team <team@soajs.org>',
             "body": [
-                'RUN apt-get update && apt-get install --fix-missing -y make g++ libssl-dev libcurl3', //NOTE: set here temporary for testing, should be moved to base image
                 'RUN mkdir -p /opt/soajs/FILES/profiles && mkdir -p /opt/soajs/deployer && mkdir -p /etc/nginx/ssl',
                 'ADD ./deployer /opt/soajs/deployer/', // new deployer
                 'RUN cd /opt/soajs/deployer/ && npm install',
@@ -29,7 +28,6 @@ module.exports = {
             "from": 'FROM soajsorg/baseservice',
             "maintainer": 'MAINTAINER SOAJS Team <team@soajs.org>',
             "body": [
-                'RUN apt-get update && apt-get install --fix-missing -y libssl-dev libcurl3', //NOTE: set here temporary for testing, should be moved to base image
                 'RUN mkdir -p /opt/soajs/node_modules && mkdir -p /opt/soajs/FILES/profiles && mkdir -p /opt/soajs/deployer',
                 'ADD ./FILES /opt/soajs/FILES/', // old deployer, will be removed
                 'ADD ./deployer /opt/soajs/deployer/', // new deployer
