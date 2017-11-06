e1bd24c7f81621298aef33fef854ba1f5376a845
Ameer Faraj <faraj.ameer@gmail.com>
2017-11-06 17:24:23 +0200

    Pushing old updates for future reference

--- config.js
+++ config.js
@@ -59,9 +59,9 @@ module.exports = {
             "body": [
                 'USER root',
                 'ADD ./FILES/filebeat/filebeat.yml /usr/share/filebeat/filebeat.yml',
-                'RUN chown -R filebeat:filebeat /usr/share/filebeat/ && \\',
-                    'mkdir -p /var/log/soajs && chmod -R a+rw /var/log/soajs',
-                'USER filebeat',
+                // 'RUN chown -R filebeat:filebeat /usr/share/filebeat/ && \\',
+                //     'mkdir -p /var/log/soajs && chmod -R a+rw /var/log/soajs',
+                // 'USER filebeat',
                 'CMD ["/bin/bash"]',
                 'ENTRYPOINT [ "/bin/bash", "-c", "filebeat -e -v -path.home /usr/share/filebeat" ]'
             ]
@@ -99,7 +99,7 @@ module.exports = {
                 'ADD ./FILES/kibana/package.json /usr/share/kibana/plugins/kibana-time-plugin/package.json',
                 'RUN mkdir -p /opt/soajs/deployer',
                 'ADD ./deployer /opt/soajs/deployer',
-                'RUN cd /opt/soajs/deployer && npm install && chown -R kibana:kibana /opt /usr/share/kibana/config',
+                'RUN cd /opt/soajs/deployer && npm install && chown -R kibana:kibana /opt /usr/share/kibana',
                 'USER kibana',
                 'CMD ["/bin/bash"]'
             ]
@@ -128,7 +128,7 @@ module.exports = {
                 'RUN ${ES_WORKDIR}/bin/elasticsearch-plugin install file://${ES_WORKDIR}/readonlyrest-1.16.11_${READONLY_REST_ES_VERSION}.zip',
                 'RUN mkdir -p /opt/soajs/deployer/',
                 'ADD ./deployer /opt/soajs/deployer/',
-                'RUN cd /opt/soajs/deployer && npm install && chown -R elasticsearch:elasticsearch /opt',
+                'RUN cd /opt/soajs/deployer && npm install && chown -R elasticsearch:elasticsearch /opt /usr/share/elasticsearch',
                 'USER elasticsearch',
                 'CMD ["/bin/sh"]'
             ]
