1afe62463b0058cf484abf6b7af5d30cb1b00bc3
antoinehage <me@antoinehage.com>
2019-10-02 16:31:11 -0400

    fixed step deploy for golang and nodejs

--- deployer/golang/index.js
+++ deployer/golang/index.js
@@ -8,108 +8,129 @@ const spawn = require('child_process').spawn;
 const utils = require('../utils');
 
 let golang = {
-
-    /**
-     * Function that checks for git environment variables and clones repository
-     * @param  {Object}   options An object that contains params passed to the function
-     * @param  {Function} cb      Callback function
-     *
-     */
-    init(options, cb) {
-
-        if (options.golang && options.golang.git && options.golang.git.domain && options.golang.git.owner && options.golang.git.repo) {
-
-            let repoDirPath = path.join(options.paths.golang.path, options.golang.git.domain, options.golang.git.owner, options.golang.git.repo);
-            options.repoDirPath = repoDirPath;
-
-            fse.ensureDir(repoDirPath, (error) => {
-                if (error) {
-                    log(`An error occured while creating ${repoDirPath} ...`);
-                    throw new Error(error);
-                }
-
-                let cloneOptions = {
-                    repo: {
-                        git: options.golang.git
-                    },
-                    clonePath: repoDirPath
-                };
-                utils.clone(cloneOptions, (error) => {
-                    if (error) throw new Error(error);
-                    return cb();
-                    //return golang.installDeps(options, cb);
-                });
-            });
-        }
-        else {
-            log(`Missing required git information, found git owner: [${options.golang.git.owner}], git repo: [${options.golang.git.repo}], exiting ...`);
-            return cb();
-        }
-    },
-
-    /**
-     * Function that installs dependencies for a golang service
-     * @param  {Object}   options An object that contains params passed to the function
-     * @param  {Function} cb      Callback function
-     *
-     */
-    installDeps(options, cb) {
-        log('Installing service dependencies ...');
-        const get = spawn('go', ['get', '-v', './...'], {stdio: 'inherit', cwd: options.repoDirPath});
-
-        get.on('data', (data) => {
-            console.log(data.toString());
-        });
-
-        get.on('close', (code) => {
-            if (code === 0) {
-                log(`go get install process exited with code: ${code}`);
-                return cb();
-                //return golang.run(options, cb);
-            }
-            else {
-                throw new Error(`go get install failed, exit code: ${code}`);
-            }
-        });
-
-        get.on('error', (error) => {
-            log('An error occured while installing dependencies');
-            throw new Error(error);
-        });
-    },
-
-    /**
-     * Function that runs a golang service
-     * @param  {Object}   options An object that contains params passed to the function
-     * @param  {Function} cb      Callback function
-     *
-     */
-    run(options, cb) {
-        log('Running service ...');
-
-        let runParams = [options.golang.main];
-
-        const go = spawn(options.golang.main, [], {stdio: 'inherit', cwd: options.repoDirPath});
-
-        go.on('data', (data) => {
-            console.log(data.toString());
-        });
-
-        go.on('close', (code) => {
-            log(`go process exited with code: ${code}`);
-            return cb();
-        });
-
-        go.on('error', (error) => {
-            log('An error occured while running service');
-            throw new Error(error);
-        });
-    }
-
+	
+	/**
+	 * Function that checks for git environment variables and clones repository
+	 * @param  {Object}   options An object that contains params passed to the function
+	 * @param  {Function} cb      Callback function
+	 *
+	 */
+	init(options, cb) {
+		
+		if (options.golang && options.golang.git && options.golang.git.domain && options.golang.git.owner && options.golang.git.repo) {
+			
+			let repoDirPath = path.join(options.paths.golang.path, options.golang.git.domain, options.golang.git.owner, options.golang.git.repo);
+			options.repoDirPath = repoDirPath;
+			
+			fse.ensureDir(repoDirPath, (error) => {
+				if (error) {
+					log(`An error occured while creating ${repoDirPath} ...`);
+					throw new Error(error);
+				}
+				
+				let cloneOptions = {
+					repo: {
+						git: options.golang.git
+					},
+					clonePath: repoDirPath
+				};
+				utils.clone(cloneOptions, (error) => {
+					if (error) throw new Error(error);
+					return cb();
+					//return golang.installDeps(options, cb);
+				});
+			});
+		}
+		else {
+			log(`Missing required git information, found git owner: [${options.golang.git.owner}], git repo: [${options.golang.git.repo}], exiting ...`);
+			return cb();
+		}
+	},
+	
+	/**
+	 * Function that installs dependencies for a golang service
+	 * @param  {Object}   options An object that contains params passed to the function
+	 * @param  {Function} cb      Callback function
+	 *
+	 */
+	installDeps(options, cb) {
+		log('Installing service dependencies ...');
+		
+		if (options.golang && options.golang.git && options.golang.git.domain && options.golang.git.owner && options.golang.git.repo) {
+			
+			let repoDirPath = path.join(options.paths.golang.path, options.golang.git.domain, options.golang.git.owner, options.golang.git.repo);
+			options.repoDirPath = repoDirPath;
+			
+			const get = spawn('go', ['get', '-v', './...'], {stdio: 'inherit', cwd: options.repoDirPath});
+			
+			get.on('data', (data) => {
+				console.log(data.toString());
+			});
+			
+			get.on('close', (code) => {
+				if (code === 0) {
+					log(`go get install process exited with code: ${code}`);
+					return cb();
+					//return golang.run(options, cb);
+				}
+				else {
+					throw new Error(`go get install failed, exit code: ${code}`);
+				}
+			});
+			
+			get.on('error', (error) => {
+				log('An error occured while installing dependencies');
+				throw new Error(error);
+			});
+		}
+		else {
+			log(`Missing required git information, found git owner: [${options.golang.git.owner}], git repo: [${options.golang.git.repo}], exiting ...`);
+			return cb();
+		}
+	},
+	
+	/**
+	 * Function that runs a golang service
+	 * @param  {Object}   options An object that contains params passed to the function
+	 * @param  {Function} cb      Callback function
+	 *
+	 */
+	run(options, cb) {
+		log('Running service ...');
+		
+		if (options.golang && options.golang.git && options.golang.git.domain && options.golang.git.owner && options.golang.git.repo) {
+			
+			let repoDirPath = path.join(options.paths.golang.path, options.golang.git.domain, options.golang.git.owner, options.golang.git.repo);
+			options.repoDirPath = repoDirPath;
+			
+			let runParams = [options.golang.main];
+			
+			const go = spawn(options.golang.main, [], {stdio: 'inherit', cwd: options.repoDirPath});
+			
+			go.on('data', (data) => {
+				console.log(data.toString());
+			});
+			
+			go.on('close', (code) => {
+				log(`go process exited with code: ${code}`);
+				return cb();
+			});
+			
+			go.on('error', (error) => {
+				log('An error occured while running service');
+				throw new Error(error);
+			});
+		}
+		else {
+			log(`Missing required git information, found git owner: [${options.golang.git.owner}], git repo: [${options.golang.git.repo}], exiting ...`);
+			return cb();
+		}
+	}
+	
 };
 
 module.exports = {
-    deploy: golang.init,
-    install: golang.installDeps,
-    run: golang.run
+	deploy: golang.init,
+	install: golang.installDeps,
+	run: golang.run
 };
--- deployer/nodejs/index.js
+++ deployer/nodejs/index.js
@@ -8,118 +8,143 @@ const spawn = require('child_process').spawn;
 const utils = require('../utils');
 
 let nodejs = {
-
-    /**
-     * Function that checks for git environment variables and clones repository
-     * @param  {Object}   options An object that contains params passed to the function
-     * @param  {Function} cb      Callback function
-     *
-     */
-    init(options, cb) {
-
-        if (options.nodejs && options.nodejs.git && options.nodejs.git.owner && options.nodejs.git.repo) {
-            if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
-                log('WARNING: Environment variable NODE_ENV is not set to "production". This is not recommended in production environments!');
-            }
-
-            let repoDirPath = path.join(options.paths.service.path, options.nodejs.git.repo);
-            fse.ensureDir(repoDirPath, (error) => {
-                if (error) {
-                    log(`An error occured while creating ${repoDirPath} ...`);
-                    throw new Error(error);
-                }
-
-                let cloneOptions = {
-                    repo: {
-                        git: options.nodejs.git
-                    },
-                    clonePath: repoDirPath
-                };
-                utils.clone(cloneOptions, (error) => {
-                    if (error) throw new Error(error);
-
-                    options.repoDirPath = repoDirPath;
-                    return cb();
-                    //return nodejs.installDeps(options, cb);
-                });
-            });
-        }
-        else {
-            log(`Missing required git information, found git owner: [${options.nodejs.git.owner}], git repo: [${options.nodejs.git.repo}], exiting ...`);
-            return cb();
-        }
-    },
-
-    /**
-     * Function that installs dependencies for a nodejs service
-     * @param  {Object}   options An object that contains params passed to the function
-     * @param  {Function} cb      Callback function
-     *
-     */
-    installDeps(options, cb) {
-        log('Installing service dependencies ...');
-        const npm = spawn('npm', [ 'install' ], { stdio: 'inherit', cwd: options.repoDirPath });
-
-        npm.on('data', (data) => {
-            console.log (data.toString());
-        });
-
-        npm.on('close', (code) => {
-            if (code === 0) {
-                log(`npm install process exited with code: ${code}`);
-                return cb()
-                //return nodejs.run(options, cb);
-            }
-            else {
-                throw new Error(`npm install failed, exit code: ${code}`);
-            }
-        });
-
-        npm.on('error', (error) => {
-            log('An error occured while installing dependencies');
-            throw new Error(error);
-        });
-    },
-
-    /**
-     * Function that runs a nodejs service
-     * @param  {Object}   options An object that contains params passed to the function
-     * @param  {Function} cb      Callback function
-     *
-     */
-    run(options, cb) {
-        log('Running service ...');
-        let serviceRun = options.nodejs.main;
-        let nodeParams = ((options.nodejs.memory) ? '--max_old_space_size=' + options.nodejs.memory : '');
-
-        let runParams = [];
-        if (nodeParams) runParams.push(nodeParams);
-        runParams.push(serviceRun);
-
-        const node = spawn('node', runParams, {
-        	stdio: 'inherit',
-	        cwd: path.join(options.repoDirPath, '/')
-        });
-
-        node.on('data', (data) => {
-            console.log (data.toString());
-        });
-
-        node.on('close', (code) => {
-            log(`node process exited with code: ${code}`);
-            return cb();
-        });
-
-        node.on('error', (error) => {
-            log('An error occured while installing dependencies');
-            throw new Error(error);
-        });
-    }
-
+	
+	/**
+	 * Function that checks for git environment variables and clones repository
+	 * @param  {Object}   options An object that contains params passed to the function
+	 * @param  {Function} cb      Callback function
+	 *
+	 */
+	init(options, cb) {
+		
+		if (options.nodejs && options.nodejs.git && options.nodejs.git.owner && options.nodejs.git.repo) {
+			if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+				log('WARNING: Environment variable NODE_ENV is not set to "production". This is not recommended in production environments!');
+			}
+			let repoDirPath = path.join(options.paths.service.path, options.nodejs.git.repo);
+			options.repoDirPath = repoDirPath;
+			
+			fse.ensureDir(repoDirPath, (error) => {
+				if (error) {
+					log(`An error occured while creating ${repoDirPath} ...`);
+					throw new Error(error);
+				}
+				
+				let cloneOptions = {
+					repo: {
+						git: options.nodejs.git
+					},
+					clonePath: repoDirPath
+				};
+				utils.clone(cloneOptions, (error) => {
+					if (error) throw new Error(error);
+					return cb();
+				});
+			});
+		}
+		else {
+			log(`Missing required git information, found git owner: [${options.nodejs.git.owner}], git repo: [${options.nodejs.git.repo}], exiting ...`);
+			return cb();
+		}
+	},
+	
+	/**
+	 * Function that installs dependencies for a nodejs service
+	 * @param  {Object}   options An object that contains params passed to the function
+	 * @param  {Function} cb      Callback function
+	 *
+	 */
+	installDeps(options, cb) {
+		log('Installing service dependencies ...');
+		
+		if (options.nodejs && options.nodejs.git && options.nodejs.git.owner && options.nodejs.git.repo) {
+			if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+				log('WARNING: Environment variable NODE_ENV is not set to "production". This is not recommended in production environments!');
+			}
+			let repoDirPath = path.join(options.paths.service.path, options.nodejs.git.repo);
+			options.repoDirPath = repoDirPath;
+			
+			const npm = spawn('npm', ['install'], {stdio: 'inherit', cwd: options.repoDirPath});
+			
+			npm.on('data', (data) => {
+				console.log(data.toString());
+			});
+			
+			npm.on('close', (code) => {
+				if (code === 0) {
+					log(`npm install process exited with code: ${code}`);
+					return cb()
+					//return nodejs.run(options, cb);
+				}
+				else {
+					throw new Error(`npm install failed, exit code: ${code}`);
+				}
+			});
+			
+			npm.on('error', (error) => {
+				log('An error occured while installing dependencies');
+				throw new Error(error);
+			});
+		}
+		else {
+			log(`Missing required git information, found git owner: [${options.nodejs.git.owner}], git repo: [${options.nodejs.git.repo}], exiting ...`);
+			return cb();
+		}
+	},
+	
+	/**
+	 * Function that runs a nodejs service
+	 * @param  {Object}   options An object that contains params passed to the function
+	 * @param  {Function} cb      Callback function
+	 *
+	 */
+	run(options, cb) {
+		log('Running service ...');
+		
+		if (options.nodejs && options.nodejs.git && options.nodejs.git.owner && options.nodejs.git.repo) {
+			if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+				log('WARNING: Environment variable NODE_ENV is not set to "production". This is not recommended in production environments!');
+			}
+			let repoDirPath = path.join(options.paths.service.path, options.nodejs.git.repo);
+			options.repoDirPath = repoDirPath;
+			
+			
+			let serviceRun = options.nodejs.main;
+			let nodeParams = ((options.nodejs.memory) ? '--max_old_space_size=' + options.nodejs.memory : '');
+			
+			let runParams = [];
+			if (nodeParams) runParams.push(nodeParams);
+			runParams.push(serviceRun);
+			
+			const node = spawn('node', runParams, {
+				stdio: 'inherit',
+				cwd: path.join(options.repoDirPath, '/')
+			});
+			
+			node.on('data', (data) => {
+				console.log(data.toString());
+			});
+			
+			node.on('close', (code) => {
+				log(`node process exited with code: ${code}`);
+				return cb();
+			});
+			
+			node.on('error', (error) => {
+				log('An error occured while installing dependencies');
+				throw new Error(error);
+			});
+		}
+		else {
+			log(`Missing required git information, found git owner: [${options.nodejs.git.owner}], git repo: [${options.nodejs.git.repo}], exiting ...`);
+			return cb();
+		}
+	}
+	
 };
 
 module.exports = {
-    deploy: nodejs.init,
-    install: nodejs.installDeps,
-    run: nodejs.run
+	deploy: nodejs.init,
+	install: nodejs.installDeps,
+	run: nodejs.run
 };
